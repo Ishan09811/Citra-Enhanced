11c53116c7e532ae3dbda31dea4e27bc4a5d71a5
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-02-02 11:30:12 +0530

    Create GameAboutAdapter.kt

---
+++ src/android/app/src/main/java/io/github/mandarine3ds/mandarine/adapters/GameAboutAdapter.kt
@@ -0,0 +1,79 @@
+// SPDX-FileCopyrightText: 2023 yuzu Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+package io.github.mandarine3ds.mandarine.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.lifecycle.LifecycleOwner
+import io.github.mandarine3ds.mandarine.databinding.CardOutlinedBinding
+import io.github.mandarine3ds.mandarine.model.GameAbout
+import io.github.mandarine3ds.mandarine.model.SubmenuGameAbout
+import io.github.mandarine3ds.mandarine.utils.ViewUtils.marquee
+import io.github.mandarine3ds.mandarine.utils.ViewUtils.setVisible
+import io.github.mandarine3ds.mandarine.utils.collect
+import io.github.mandarine3ds.mandarine.viewholder.AbstractViewHolder
+
+class GamePropertiesAdapter(
+    private val viewLifecycle: LifecycleOwner,
+    private var properties: List<GameAbout>
+) : AbstractListAdapter<GameAbout, AbstractViewHolder<GameAbout>>(properties) {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): AbstractViewHolder<GameAbout> {
+        val inflater = LayoutInflater.from(parent.context)
+        return when (viewType) {
+            PropertyType.Submenu.ordinal -> {
+                SubmenuGameAboutViewHolder(
+                    CardOutlinedBinding.inflate(
+                        inflater,
+                        parent,
+                        false
+                    )
+                )
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return PropertyType.Submenu.ordinal 
+    }
+
+    inner class SubmenuGameAboutViewHolder(val binding: CardSimpleOutlinedBinding) :
+        AbstractViewHolder<GameAbout>(binding) {
+        override fun bind(model: GameAbout) {
+            val submenuInfo = model as SubmenuGameAbout
+
+            binding.root.setOnClickListener {
+                submenuInfo.action.invoke()
+            }
+
+            binding.title.setText(submenuInfo.titleId)
+            binding.description.setText(submenuInfo.descriptionId)
+            binding.icon.setImageDrawable(
+                ResourcesCompat.getDrawable(
+                    binding.icon.context.resources,
+                    submenuInfo.iconId,
+                    binding.icon.context.theme
+                )
+            )
+
+            binding.details.marquee()
+            if (submenuInfo.details != null) {
+                binding.details.setVisible(true)
+                binding.details.text = submenuProperty.details.invoke()
+            } else if (submenuInfo.detailsFlow != null) {
+                binding.details.setVisible(true)
+                submenuInfo.detailsFlow.collect(viewLifecycle) { binding.details.text = it }
+            } else {
+                binding.details.setVisible(false)
+            }
+        }
+    }
+
+    enum class PropertyType {
+        Submenu
+    }
+}
