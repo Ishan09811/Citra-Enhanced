b996601b32c2455686a07f93b4d6854fd598290c
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-02-06 19:20:27 +0530

    Create AddonsHelper.kt

---
+++ src/android/app/src/main/java/io/github/mandarine3ds/mandarine/utils/AddonsHelper.kt
@@ -0,0 +1,63 @@
+// Copyright 2025 Mandarine / Mandarine Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+package io.github.mandarine3ds.mandarine.utils
+
+import android.content.SharedPreferences
+import android.net.Uri
+import androidx.preference.PreferenceManager
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import io.github.mandarine3ds.mandarine.MandarineApplication
+import io.github.mandarine3ds.mandarine.model.Mod
+import java.io.IOException
+
+object AddonsHelper {
+    const val KEY_MODS = "Mods"
+
+    private lateinit var preferences: SharedPreferences
+
+    fun getMods(): List<Mod> {
+        val mods = mutableListOf<Mod>()
+        val context = MandarineApplication.appContext
+        preferences = PreferenceManager.getDefaultSharedPreferences(context)
+        val serializedMods = preferences.getStringSet(KEY_MODS, emptySet()) ?: emptySet()
+        mods = serializedMods.map { Json.decodeFromString<Mods>(it) }.toMutableList()
+        mods.forEach { mod ->
+          if (!FileUtil.exists(mod.installedPath)) mods.remove(mod)
+        }
+        return mods.toList()
+    } 
+
+    fun getMod(uri: Uri, installedPath: Uri, title: String? = null): Mod {
+        val filePath = uri.toString()
+
+        val newMod = Mod(
+            title ?: FileUtil.getFilename(uri)).replace("[\\t\\n\\r]+".toRegex(), " "),
+            filePath,
+            if (FileUtil.isNativePath(filePath)) {
+                MandarineApplication.documentsTree.getFilename(filePath)
+            } else {
+                FileUtil.getFilename(Uri.parse(filePath))
+            },
+            installedPath.toString()
+        )
+
+        return newMod
+    }
+
+    fun addMod(uri: Uri, installedPath: Uri, title: String? = null) {
+        preferences = PreferenceManager.getDefaultSharedPreferences(MandarineApplication.appContext)
+        val serializedMods = preferences.getStringSet(KEY_MODS, emptySet()) ?: emptySet()
+        val mods = serializedMods.map { Json.decodeFromString<Mod>(it) }.toMutableList()
+        mods.add(getMod(uri, installedPath, title))
+        val newSerializedMods = mutableSetOf<String>()
+        mods.forEach { newSerializedMods.add(Json.encodeToString(it)) }
+
+        preferences.edit()
+            .remove(KEY_MODS)
+            .putStringSet(KEY_MODS, newSerializedMods)
+            .apply()
+    }
+}
