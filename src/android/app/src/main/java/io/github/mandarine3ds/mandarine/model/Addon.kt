4a262a28f1cedcbcf014ee1bfe646f7d6acf70ca
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-02-07 18:14:45 +0530

    Update Addon.kt

--- src/android/app/src/main/java/io/github/mandarine3ds/mandarine/model/Addon.kt
+++ src/android/app/src/main/java/io/github/mandarine3ds/mandarine/model/Addon.kt
@@ -4,47 +4,28 @@
 
 package io.github.mandarine3ds.mandarine.model
 
-import android.os.Parcel
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
-@Serializable
-open class Addon(
-    var title: String = ""
-) : Parcelable {
-    constructor(parcel: Parcel) : this(parcel.readString() ?: "")
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(title)
-    }
-
-    override fun describeContents(): Int = 0
-
-    companion object CREATOR : Parcelable.Creator<Addon> {
-        override fun createFromParcel(parcel: Parcel): Addon = Addon(parcel)
-        override fun newArray(size: Int): Array<Addon?> = arrayOfNulls(size)
-    }
+interface Addon {
+    var title: String
 }
 
-@Parcelize
 @Serializable
-class Mod(
-    title: String = "",
+data class Mod(
+    override var title: String = "",
     val path: String = "",
     val filename: String = "",
     val installedPath: String = "",
     val titleId: Int = 0,
     val enabled: Boolean = true
-) : Addon(title)
+) : Addon
 
-@Parcelize
 @Serializable
-class CustomTexture(
-    title: String = "",
+data class CustomTexture(
+    override var title: String = "",
     val path: String = "",
     val filename: String = "",
     val installedPath: String = "",
     val titleId: Int = 0,
     val enabled: Boolean = true
-) : Addon(title)
+) : Addon
