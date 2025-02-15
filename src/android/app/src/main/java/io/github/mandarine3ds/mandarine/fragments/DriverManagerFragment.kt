baef0e812103190a9dc6198dfa4ce57f3f3b94d7
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-02-15 14:06:05 +0530

    Update DriverManagerFragment.kt

--- src/android/app/src/main/java/io/github/mandarine3ds/mandarine/fragments/DriverManagerFragment.kt
+++ src/android/app/src/main/java/io/github/mandarine3ds/mandarine/fragments/DriverManagerFragment.kt
@@ -10,6 +10,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
